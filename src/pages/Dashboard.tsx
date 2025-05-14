69e54f746b91c39d0118c2d0fb9c371b95c84552
M35KA <arnas.penkauskas@gmail.com>
2025-05-14 21:21:51 +0300

    navigation

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -136,7 +136,8 @@ const Dashboard = () => {
               <div className="flex justify-between items-center mb-6">
                 <h1 className="text-2xl font-bold">Dashboard</h1>
                 <Button className="rounded-none">
-                  <Plus className="h-4 w-4 mr-2" /> New Listing
+                  <Plus className="h-4 w-4 mr-2" />{" "}
+                  <Link to="/create-listing">New Listing</Link>
                 </Button>
               </div>
 
